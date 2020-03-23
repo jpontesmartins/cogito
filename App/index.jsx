f93067cc382ccf3433e501de5600bd875af00719
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-03-23 10:28:11 -0300

    first

---
+++ App/index.jsx
@@ -0,0 +1,54 @@
+import React, { Component, Fragment } from "react";
+
+import "./styles.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="container">
+          <div className="column1">
+            <div className="card">
+              <div className="card-date">
+                1882
+              </div>
+              <div className="card-event">
+                Nascimento de James Joyce
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="card-date">
+                1828
+             </div>
+              <div className="card-event">
+                Nascimento de Lev Tolstoi
+            </div>
+            </div>
+
+            <div className="card">
+              <div className="card-date">
+                1899
+               </div>
+              <div className="card-event">
+                Nascimento de Ernest Hemingway
+              </div>
+            </div>
+          </div>
+
+          <div className="map-coluna">
+            Mapa
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
