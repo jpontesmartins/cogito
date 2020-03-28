9e615fd914a5c56e086f57c27d01df1a5c6883e5
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-03-28 14:44:57 -0300

    alterada a cor do marcardor

--- src/App/MarkerDefault.jsx
+++ src/App/MarkerDefault.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Marker } from "react-leaflet";
+import L from "leaflet";
 
 export default function MarkerDefault(props) {
+
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
     const initMarker = ref => {
         if (ref) {
             ref.leafletElement.openPopup();
         }
     }
-    return <Marker ref={initMarker} {...props} />
+    return <Marker ref={initMarker} {...props} icon={greenIcon} />
 }
