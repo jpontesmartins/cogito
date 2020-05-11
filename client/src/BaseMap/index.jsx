b9b858cd5603cac530c7872e3fdc2c47c4ae026d
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-11 08:00:23 -0300

    removidos alguns Fragments

--- client/src/BaseMap/index.jsx
+++ client/src/BaseMap/index.jsx
@@ -13,11 +13,11 @@ class BaseMap extends Component {
             markers: {
                 "type": "FeatureCollection",
                 "features": []
-              }
+            }
         };
     }
 
-    componentDidUpdate(prevProps) {    }
+    componentDidUpdate(prevProps) { }
 
 
     render() {
@@ -25,7 +25,7 @@ class BaseMap extends Component {
         const SUL = [-27.590354384999955, -48.556835725999974];
         const SIZE = { height: "100%", width: "100%" };
         const urlTileLayer = "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png";
-        
+
         const { markers } = this.state;
 
         let position = [53.311667, -10.174333]
@@ -33,48 +33,47 @@ class BaseMap extends Component {
         const { birthLocal, deathLocal } = this.props;
 
         let birth = {
-            coordinates: [0,0],
+            coordinates: [0, 0],
             name: "Birth"
         }
         let death = {
-            coordinates: [10,10],
+            coordinates: [10, 10],
             name: "Death"
         };
-        
+
         if (birthLocal) {
             birth = birthLocal;
             death = deathLocal;
         }
 
         return (
-            <Fragment>
-                <div className="map-coluna">
-                    <Map
-                        style={SIZE}
-                        zoomControl={false}
-                        zoom={5} center={SUL}
-                        onClick={this.clickOnMap}>
+            <div className="map-coluna">
+                <Map
+                    style={SIZE}
+                    zoomControl={false}
+                    zoom={5} center={SUL}
+                    onClick={this.clickOnMap}>
 
-                        <TileLayer url={urlTileLayer} />
+                    <TileLayer url={urlTileLayer} />
 
-                        <GeoJSON data={markers}
-                            key={hash(markers)}
-                            style={{ color: "#000" }}>
+                    <GeoJSON data={markers}
+                        key={hash(markers)}
+                        style={{ color: "#000" }}>
 
-                            {/* <SpatialInfo /> */}
+                        {/* <SpatialInfo /> */}
 
-                            <MarkerDefault position={birth.coordinates}>
-                                <Content title={birth.name} />
-                            </MarkerDefault>
-                            <MarkerDefault position={death.coordinates}>
-                                <Content title={death.name} />
-                            </MarkerDefault>
-                           
-                        </GeoJSON>
+                        <MarkerDefault position={birth.coordinates}>
+                            <Content title={birth.name} />
+                        </MarkerDefault>
+                       
+                        <MarkerDefault position={death.coordinates}>
+                            <Content title={death.name} />
+                        </MarkerDefault>
 
-                    </Map>
-                </div>
-            </Fragment>
+                    </GeoJSON>
+
+                </Map>
+            </div>
         );
     }
 }
